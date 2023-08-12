4f041e0a189645bbcd930d8f1141b2889fe94a81
muratmnz <minazzmurat@gmail.com>
2023-08-12 23:03:24 +0300

    feat : designed UI and added login feature to connect and segue to home now.

---
+++ LoginScreen-Firebase/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  LoginScreen-Firebase
+//
+//  Created by Murat Minaz on 12.08.2023.
+//
+
+struct C {
+    static let loginSegue = "loginSegue"
+}
--- LoginScreen-Firebase/ViewController.swift
+++ LoginScreen-Firebase/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseCore
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginText: UITextField!
+    
+    @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    
+    //login with email & password. Segue to home page.
+    @IBAction func pressLogin(_ sender: UIButton) {
+        
+        if let email = loginText.text, let password = passwordText.text {
+            Auth.auth().signIn(withEmail: email, password: password){
+                authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                }else{
+                    self.performSegue(withIdentifier: C.loginSegue, sender: self)
+                }
+            }
+        }
+        
+    }
+    
+    @IBAction func pressRegister(_ sender: UIButton) {
+    }
 }
 
