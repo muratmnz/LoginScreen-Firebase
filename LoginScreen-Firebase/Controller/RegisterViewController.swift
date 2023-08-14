cd9f8e607e967ce4149c0a16e09a970561f2a7cd
muratmnz <minazzmurat@gmail.com>
2023-08-14 22:26:51 +0300

    feat : added and connected VCs to all Views.

---
+++ LoginScreen-Firebase/Controller/RegisterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterViewController.swift
+//  LoginScreen-Firebase
+//
+//  Created by Murat Minaz on 14.08.2023.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
