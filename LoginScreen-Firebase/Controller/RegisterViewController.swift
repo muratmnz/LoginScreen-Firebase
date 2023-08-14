58f5867629a80aeedf824cb466da7ff87814efd1
muratmnz <minazzmurat@gmail.com>
2023-08-14 23:05:22 +0300

    feat : added register feature using Firebase.

--- LoginScreen-Firebase/Controller/RegisterViewController.swift
+++ LoginScreen-Firebase/Controller/RegisterViewController.swift
@@ -6,13 +6,36 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseCore
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func pressRegister(_ sender: Any) {
+        
+        if let email = usernameTextField.text, let password = passTextField.text {
+            Auth.auth().createUser(withEmail: email, password: password){
+                authResult, error in
+                if let e = error {
+                    //error
+                    print(e.localizedDescription)
+                }else{
+                    //navigation to homeViewController after register
+                    self.performSegue(withIdentifier: C.registerSegue, sender: self)
+                }
+            }
+        }
+    }
+    
+    
 }
